bc826701f29333a6c09c6729eb9173dc52d6fe97
Chiang Lynn <lynnchiangtw@gmail.com>
2021-09-14 17:17:27 +0800

    Set up navbar, sidebar and footer sections

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import logo from '../assets/logo.svg';
 import { FaBars } from 'react-icons/fa';
+import logo from '../assets/logo.svg';
 import { Link } from 'react-router-dom';
 import { links } from '../utils/constants';
 import CartButtons from './CartButtons';
@@ -9,70 +9,100 @@ import { useProductsContext } from '../context/products_context';
 import { useUserContext } from '../context/user_context';
 
 const Nav = () => {
-  return <h4>navbar</h4>;
+  return (
+    <NavContainer>
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="comfy sloth" />
+          </Link>
+          <button type="button" className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          {links.map(({ id, text, url }) => <li key={id}><Link to={url}>{text}</Link></li>)}
+        </ul>
+        <CartButtons />
+      </div>
+    </NavContainer>
+  );
 };
 
 const NavContainer = styled.nav`
-  height: 5rem;
   display: flex;
   align-items: center;
   justify-content: center;
+  height: 5rem;
 
   .nav-center {
-    width: 90vw;
     margin: 0 auto;
+    width: 90vw;
     max-width: var(--max-width);
   }
+
   .nav-header {
     display: flex;
-    align-items: center;
     justify-content: space-between;
+    align-items: center;
+
     img {
-      width: 175px;
       margin-left: -15px;
+      width: 175px;
     }
   }
+
   .nav-toggle {
-    background: transparent;
-    border: transparent;
     color: var(--clr-primary-5);
+    border: transparent;
+    background: transparent;
     cursor: pointer;
+
     svg {
       font-size: 2rem;
     }
   }
+
   .nav-links {
     display: none;
   }
+
   .cart-btn-wrapper {
     display: none;
   }
+
   @media (min-width: 992px) {
     .nav-toggle {
       display: none;
     }
+
     .nav-center {
       display: grid;
       grid-template-columns: auto 1fr auto;
       align-items: center;
     }
+
     .nav-links {
       display: flex;
       justify-content: center;
+
       li {
-        margin: 0 0.5rem;
+        margin: 0 .5rem;
       }
+
       a {
+        padding: .5rem;
         color: var(--clr-grey-3);
         font-size: 1rem;
         text-transform: capitalize;
         letter-spacing: var(--spacing);
-        padding: 0.5rem;
+
         &:hover {
           border-bottom: 2px solid var(--clr-primary-7);
         }
       }
     }
+
     .cart-btn-wrapper {
       display: grid;
     }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,61 +1,83 @@
 import React from 'react';
+import styled from 'styled-components';
+import { FaTimes } from 'react-icons/fa';
 import logo from '../assets/logo.svg';
 import { Link } from 'react-router-dom';
-import { useProductsContext } from '../context/products_context';
-import { FaTimes } from 'react-icons/fa';
 import { links } from '../utils/constants';
-import styled from 'styled-components';
 import CartButtons from './CartButtons';
+import { useProductsContext } from '../context/products_context';
 import { useUserContext } from '../context/user_context';
 
 const Sidebar = () => {
-  return <h4>sidebar</h4>;
+  const isOpen = true;
+
+  return (
+    <SidebarContainer>
+      <aside className={`sidebar ${isOpen && 'show-sidebar'}`}>
+        <div className="sidebar-header">
+          <img src={logo} alt="comfy sloth" className="logo" />
+          <button type="button" className="close-btn">
+            <FaTimes />
+          </button>
+        </div>
+        <ul className="links">
+          {links.map(({ id, text, url }) => <li key={id}><Link to={url}>{text}</Link></li>)}
+          <li><Link to="/checkout">checkout</Link></li>
+        </ul>
+        <CartButtons />
+      </aside>
+    </SidebarContainer >
+  );
 };
 
 const SidebarContainer = styled.div`
   text-align: center;
+
   .sidebar-header {
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 1rem 1.5rem;
   }
+
   .close-btn {
+    margin-top: 0.2rem;
     font-size: 2rem;
-    background: transparent;
+    color: var(--clr-red-dark);
     border-color: transparent;
-    color: var(--clr-primary-5);
+    background: transparent;
     transition: var(--transition);
     cursor: pointer;
-    color: var(--clr-red-dark);
-    margin-top: 0.2rem;
   }
+
   .close-btn:hover {
     color: var(--clr-red-light);
   }
+
   .logo {
     justify-self: center;
     height: 45px;
   }
+
   .links {
     margin-bottom: 2rem;
   }
+
   .links a {
     display: block;
-    text-align: left;
-    font-size: 1rem;
-    text-transform: capitalize;
     padding: 1rem 1.5rem;
+    font-size: 1rem;
     color: var(--clr-grey-3);
-    transition: var(--transition);
+    text-align: left;
+    text-transform: capitalize;
     letter-spacing: var(--spacing);
+    transition: var(--transition);
   }
 
   .links a:hover {
-    padding: 1rem 1.5rem;
-    padding-left: 2rem;
-    background: var(--clr-grey-10);
+    padding: 1rem 1.5rem 1rem 2rem;
     color: var(--clr-grey-2);
+    background: var(--clr-grey-10);
   }
 
   .sidebar {
@@ -65,17 +87,20 @@ const SidebarContainer = styled.div`
     width: 100%;
     height: 100%;
     background: var(--clr-white);
-    transition: var(--transition);
     transform: translate(-100%);
+    transition: var(--transition);
     z-index: -1;
   }
+
   .show-sidebar {
     transform: translate(0);
     z-index: 999;
   }
+
   .cart-btn-wrapper {
     margin: 2rem auto;
   }
+
   @media screen and (min-width: 992px) {
     .sidebar {
       display: none;
