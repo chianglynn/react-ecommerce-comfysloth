f1339c1552825e5386fd4d8d8cb185dcb26492b2
Chiang Lynn <lynnchiangtw@gmail.com>
2021-09-15 15:51:54 +0800

    Finish about page

--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -2,23 +2,32 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const PageHero = () => {
-  return <h4>page hero</h4>;
+const PageHero = ({ title }) => {
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <h3>
+          <Link to="/">home</Link> / {title}
+        </h3>
+      </div>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.section`
-  background: var(--clr-primary-10);
-  width: 100%;
-  min-height: 20vh;
   display: flex;
   align-items: center;
-
+  width: 100%;
   color: var(--clr-primary-1);
+  min-height: 20vh;
+  background: var(--clr-primary-10);
+
   a {
-    color: var(--clr-primary-3);
     padding: 0.5rem;
+    color: var(--clr-primary-3);
     transition: var(--transition);
   }
+
   a:hover {
     color: var(--clr-primary-1);
   }
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,35 +1,53 @@
 import React from 'react';
 import styled from 'styled-components';
-import { PageHero } from '../components';
 import aboutImg from '../assets/hero-bcg.jpeg';
+import { PageHero } from '../components';
 
 const AboutPage = () => {
-  return <h4>about page</h4>;
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quaerat nihil maiores itaque iusto blanditiis nostrum ut, voluptate fuga similique esse dicta cupiditate necessitatibus quisquam odio? Quia quidem voluptatum fugit?</p>
+        </article>
+      </Wrapper>
+    </main>
+  );
 };
 
 const Wrapper = styled.section`
   display: grid;
   gap: 4rem;
+
   img {
-    width: 100%;
     display: block;
-    border-radius: var(--radius);
+    width: 100%;
     height: 500px;
+    border-radius: var(--radius);
     object-fit: cover;
   }
+
   p {
-    line-height: 2;
+    margin: 2rem auto 0;
     max-width: 45em;
-    margin: 0 auto;
-    margin-top: 2rem;
     color: var(--clr-grey-5);
+    line-height: 2;
   }
+
   .title {
     text-align: left;
   }
+
   .underline {
     margin-left: 0;
   }
+
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
