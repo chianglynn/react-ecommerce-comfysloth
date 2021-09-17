132ab502ee49ebbb230e1c118a62127e05141b5b
Chiang Lynn <lynnchiangtw@gmail.com>
2021-09-17 20:24:00 +0800

    Update featured products section of homepage

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -7,25 +7,44 @@ import Loading from './Loading';
 import Product from './Product';
 
 const FeaturedProducts = () => {
-  return <h4>featured products</h4>;
+  const { products_loading: loading, products_error: error, featured_products: featured } = useProductsContext();
+
+  if (loading) return <Loading />;
+  if (error) return <Error />;
+
+  return (
+    <Wrapper className="section">
+      <div className="title">
+        <h2>featured products</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="section-center featured">
+        {featured.slice(0, 3).map(product => <Product key={product.id} {...product} />)}
+      </div>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.section`
   background: var(--clr-grey-10);
+
   .featured {
-    margin: 4rem auto;
     display: grid;
     gap: 2.5rem;
+    margin: 4rem auto;
+
     img {
       height: 225px;
     }
   }
+
   .btn {
     display: block;
-    width: 148px;
     margin: 0 auto;
+    width: 148px;
     text-align: center;
   }
+
   @media (min-width: 576px) {
     .featured {
       grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,58 +1,76 @@
 import React from 'react';
 import styled from 'styled-components';
-import { formatPrice } from '../utils/helpers';
 import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { formatPrice } from '../utils/helpers';
 
-const Product = () => {
-  return <h4>product</h4>;
+const Product = ({ id, name, price, image }) => {
+  return (
+    <Wrapper>
+      <div className="container">
+        <img src={image} alt={name} />
+        <Link to={`/products/${id}`} className="link"><FaSearch /></Link>
+      </div>
+      <footer>
+        <h5>{name}</h5>
+        <p>{formatPrice(price)}</p>
+      </footer>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.article`
   .container {
     position: relative;
-    background: var(--clr-black);
     border-radius: var(--radius);
+    background: var(--clr-black);
   }
+
   img {
-    width: 100%;
     display: block;
-    object-fit: cover;
+    width: 100%;
     border-radius: var(--radius);
     transition: var(--transition);
+    object-fit: cover;
   }
+
   .link {
     position: absolute;
     top: 50%;
     left: 50%;
-    transform: translate(-50%, -50%);
-    background: var(--clr-primary-5);
     display: flex;
     align-items: center;
     justify-content: center;
     width: 2.5rem;
     height: 2.5rem;
     border-radius: 50%;
+    background: var(--clr-primary-5);
+    transform: translate(-50%, -50%);
     transition: var(--transition);
     opacity: 0;
     cursor: pointer;
+
     svg {
       font-size: 1.25rem;
       color: var(--clr-white);
     }
   }
+
   .container:hover img {
     opacity: 0.5;
   }
+
   .container:hover .link {
     opacity: 1;
   }
+
   footer {
-    margin-top: 1rem;
     display: flex;
     justify-content: space-between;
     align-items: center;
+    margin-top: 1rem;
   }
+
   footer h5,
   footer p {
     margin-bottom: 0;
